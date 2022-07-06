0a802ae1904ae9ec979f9917944ad53544f9c3dd
Andrzej Kulesza <andykrk22@gmail.com>
2022-07-06 19:49:10 +0200

    version 1.04a

--- contact/content.php
+++ contact/content.php
@@ -7,14 +7,12 @@
     <label for="sub1">Pomysł</label><br>
     <input type="radio" id="sub2" name="subject" value="Uwaga" required>
     <label for="sub2">Uwaga</label><br>
-    <input type="radio" id="sub3" name="subject" value="Wniosek" required>
-    <label for="sub3">Wniosek</label><br>
-    <input type="radio" id="sub4" name="subject" value="Błąd" required>
-    <label for="sub4">Błąd w partii</label><br>
-    <input type="radio" id="sub5" name="subject" value="Brak" required>
-    <label for="sub5">Brakująca partia</label><br>
-    <input type="radio" id="sub6" name="subject" value="Inne" required>
-    <label for="sub6">Inne</label><br>
+    <input type="radio" id="sub3" name="subject" value="Błąd" required>
+    <label for="sub3">Błąd w partii</label><br>
+    <input type="radio" id="sub4" name="subject" value="Brak" required>
+    <label for="sub4">Brakująca partia</label><br>
+    <input type="radio" id="sub5" name="subject" value="Inne" required>
+    <label for="sub5">Inne</label><br>
     <h4>Treść: </h4>
     <textarea rows="6" cols="50" name="content" form="form" placeholder="Wpisz tekst..." ></textarea><br>
 
--- contact/send.php
+++ contact/send.php
@@ -3,7 +3,7 @@
     include '../menu.php';
     echo "<div id='content'>";
     if( isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['subject']) && !empty($_POST['subject']) 
-    && isset($_POST['content']) && !empty($_POST['content']) && trim($_POST['content']) != "Wpisz tekst..." ){
+    && isset($_POST['content']) && !empty($_POST['content'])){
         $receiver = "andykrk22@gmail.com";
         $subject = $_POST['subject'];
         $mssg = $_POST['content']."\n kontakt: ".$_POST['email']."\n wygenerowano przez bazaszachowa.smallhost.pl  ".date("Y.m.d H:i:s");
