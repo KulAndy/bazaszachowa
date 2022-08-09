a5341db95e3c3038a4378d62cccc121c50430526
Andrzej Kulesza <andykrk22@gmail.com>
2022-08-09 18:15:27 +0200

    version 1.23

--- search/search.php
+++ search/search.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 require 'login_data.inc';
 @$db = new mysqli($host, $user, $password, $base);
@@ -45,82 +45,163 @@ if (isset($_POST['maxEco']) && !empty($_POST['maxEco'])) {
 if (isset($_POST['searching'])) {
     if ($_POST['searching'] == 'classic') {
         if (isset($white)) {
-            $white = $white."%";
+            $white = $white . "%";
+            $white_players = $db->prepare("SELECT fullname FROM $players_table WHERE fullname like ?");
+            $white_players->bind_param('s', $white);
+            $white_players->execute();
+            $result_white = $white_players->get_result();
+            $whites = $result_white->fetch_all();
         }
         if (isset($black)) {
-            $black = $black."%";
+            $black = $black . "%";
+            $black_players = $db->prepare("SELECT fullname FROM $players_table WHERE fullname like ?");
+            $black_players->bind_param('s', $black);
+            $black_players->execute();
+            $result_black = $black_players->get_result();
+            $blacks = $result_black->fetch_all();
         }
-        $query = "SELECT id, moves, Event,Site, Year,Month, Day,Round, White, Black, Result, WhiteElo, BlackElo, ECO  FROM $table WHERE ";
         $toBind = array();
-        if (isset($white)) {
-                $query = $query . " ( WhiteID in (SELECT id FROM $players_table WHERE fullname like ? ))";
-                array_push($toBind, "\$white");
-        }
-        if (isset($black)) {
-            if (sizeof($toBind) > 0) {
-                $query = $query . " and";
-            }
-                $query = $query . " BlackID in ( SELECT id FROM $players_table WHERE fullname like ? )";
-                array_push($toBind, "\$black");
-        }
-        if (isset($minYear) && isset($maxYear) && $minYear != 1475 && $maxYear != date("Y")) {
-            if (sizeof($toBind) > 0) {
-                $query = $query . " and";
-            }
-            $query = $query . " Year BETWEEN ? and ? ";
-            array_push($toBind, "\$minYear", "\$maxYear");
-        }
-        if (isset($event)) {
-            if (sizeof($toBind) > 0) {
-                $query = $query . " and";
-            }
-            $query = $query . " Event like ?";
-            array_push($toBind, "\$event");
-        }
-        if (isset($minEco) && isset($maxEco) && $minEco != "A00" && $maxEco != "E99") {
-            if (sizeof($toBind) > 0) {
-                $query = $query . " and";
-            }
-            $query = $query . " CONV(eco, 16, 10) BETWEEN CONV( ?, 16, 10) AND CONV( ?, 16, 10)";
-            array_push($toBind, "\$minEco", "\$maxEco");
-        }
-        if( isset($ignore) && $ignore == "true"){
-            $toBindSize = sizeof($toBind);
-            $query = $query . "UNION ALL\nSELECT id, moves, Event,Site, Year,Month, Day,Round, White, Black, Result, WhiteElo, BlackElo, ECO  FROM $table WHERE ";
-            if (isset($white)) {
-                    $query = $query . " ( BlackID in (SELECT id FROM $players_table WHERE fullname like ? ))";
-                    array_push($toBind, "\$white");
-            }
-            if (isset($black)) {
-                if (sizeof($toBind) > $toBindSize) {
-                    $query = $query . " and";
+        $query = "";
+        if (isset($white) && isset($black)) {
+            if (sizeof($whites) > 0 && sizeof($blacks) > 0) {
+                for ($i = 0; $i < sizeof($whites); $i++) {
+                    for ($j = 0; $j < sizeof($blacks); $j++) {
+                        if ($i > 0 || $j > 0) {
+                            $query .= "\nUNION distinct\n";
+                        }
+                        $updateQuery = "\$query .= 'SELECT id, moves, Event,Site, Year,Month, Day,Round, White, Black, Result, WhiteElo, BlackElo, ECO  
+                        FROM $table WHERE match(white) against(\'+" .
+                            str_replace(" ", " +", $whites[$i][0])
+                            . "\' in boolean mode) and match(black) against(\'+" .
+                            str_replace(" ", " +", $blacks[$j][0])
+                            . "\' in boolean mode)' ;";
+                        eval($updateQuery);
+                        if (isset($minYear) && isset($maxYear) && $minYear != 1475 && $maxYear != date("Y")) {
+                            $query = $query . " and Year BETWEEN ? and ? ";
+                        }
+                        if (isset($event)) {
+                            $query = $query . " and Event like ?";
+                        }
+                        if (isset($minEco) && isset($maxEco) && $minEco != "A00" && $maxEco != "E99") {
+                            $query = $query . " and CONV(eco, 16, 10) BETWEEN CONV( ?, 16, 10) AND CONV( ?, 16, 10)";
+                        }
+                    }
                 }
-                    $query = $query . " WhiteID in ( SELECT id FROM $players WHERE fullname like ? )";
-                    array_push($toBind, "\$black");
-            }
-            if (isset($minYear) && isset($maxYear) && $minYear != 1475 && $maxYear != date("Y")) {
-                if (sizeof($toBind) > $toBindSize) {
-                    $query = $query . " and";
+                if (isset($ignore) && $ignore == "true") {
+                    for ($i = 0; $i < sizeof($whites); $i++) {
+                        for ($j = 0; $j < sizeof($blacks); $j++) {
+                            $updateQuery = "\$query .= '\nUNION distinct\nSELECT id, moves, Event,Site, Year,Month, Day,Round, White, Black, Result, WhiteElo, BlackElo, ECO  
+                            FROM $table WHERE match(white) against(\'+" .
+                                str_replace(" ", " +", $blacks[$j][0])
+                                . "\' in boolean mode) and match(black) against(\'+" .
+                                str_replace(" ", " +", $whites[$i][0])
+                                . "\' in boolean mode)' ;";
+                            eval($updateQuery);
+                            if (isset($minYear) && isset($maxYear) && $minYear != 1475 && $maxYear != date("Y")) {
+                                $query = $query . " and Year BETWEEN ? and ? ";
+                            }
+                            if (isset($event)) {
+                                $query = $query . " and Event like ?";
+                            }
+                            if (isset($minEco) && isset($maxEco) && $minEco != "A00" && $maxEco != "E99") {
+                                $query = $query . " and CONV(eco, 16, 10) BETWEEN CONV( ?, 16, 10) AND CONV( ?, 16, 10)";
+                            }
+                        }
+                    }
                 }
-                $query = $query . " Year BETWEEN ? and ? ";
-                array_push($toBind, "\$minYear", "\$maxYear");
+            } else {
+                $query = "SELECT null as id, null as moves, null as Event,null as Site, null as Year, null as Month, null as Day, null as Round,";
+                $query .= "null as White, null as Black, null as Result, null as WhiteElo, null as BlackElo, null as ECO";
             }
-            if (isset($event)) {
-                if (sizeof($toBind) > $toBindSize) {
-                    $query = $query . " and";
+        } else if (isset($white)) {
+            if (sizeof($whites) > 0) {
+                for ($i = 0; $i < sizeof($whites); $i++) {
+                    if ($i > 0) {
+                        $query .= "\nUNION distinct\n";
+                    }
+                    $updateQuery = "\$query .= 'SELECT id, moves, Event,Site, Year,Month, Day,Round, White, Black, Result, WhiteElo, BlackElo, ECO  
+                        FROM $table WHERE match(white) against(\'+" .
+                        str_replace(" ", " +", $whites[$i][0])
+                        . "\' in boolean mode)' ;";
+                    eval($updateQuery);
+                    if (isset($minYear) && isset($maxYear) && $minYear != 1475 && $maxYear != date("Y")) {
+                        $query = $query . " and Year BETWEEN ? and ? ";
+                    }
+                    if (isset($event)) {
+                        $query = $query . " and Event like ?";
+                    }
+                    if (isset($minEco) && isset($maxEco) && $minEco != "A00" && $maxEco != "E99") {
+                        $query = $query . " and CONV(eco, 16, 10) BETWEEN CONV( ?, 16, 10) AND CONV( ?, 16, 10)";
+                    }
                 }
-                $query = $query . " Event like ?";
-                array_push($toBind, "\$event");
-            }
-            if (isset($minEco) && isset($maxEco) && $minEco != "A00" && $maxEco != "E99") {
-                if (sizeof($toBind) > $toBindSize) {
-                    $query = $query . " and";
+                if (isset($ignore) && $ignore == "true") {
+                    for ($i = 0; $i < sizeof($whites); $i++) {
+                        $updateQuery = "\$query .= '\nUNION distinct\nSELECT id, moves, Event,Site, Year,Month, Day,Round, White, Black, Result, WhiteElo, BlackElo, ECO  
+                            FROM $table WHERE match(black) against(\'+" .
+                            str_replace(" ", " +", $whites[$i][0])
+                            . "\' in boolean mode)' ;";
+                        eval($updateQuery);
+                        if (isset($minYear) && isset($maxYear) && $minYear != 1475 && $maxYear != date("Y")) {
+                            $query = $query . " and Year BETWEEN ? and ? ";
+                        }
+                        if (isset($event)) {
+                            $query = $query . " and Event like ?";
+                        }
+                        if (isset($minEco) && isset($maxEco) && $minEco != "A00" && $maxEco != "E99") {
+                            $query = $query . " and CONV(eco, 16, 10) BETWEEN CONV( ?, 16, 10) AND CONV( ?, 16, 10)";
+                        }
+                    }
                 }
-                $query = $query . " CONV(eco, 16, 10) BETWEEN CONV( ?, 16, 10) AND CONV( ?, 16, 10)";
-                array_push($toBind, "\$minEco", "\$maxEco");
+            } else {
+                $query = "SELECT null as id, null as moves, null as Event,null as Site, null as Year, null as Month, null as Day, null as Round,";
+                $query .= "null as White, null as Black, null as Result, null as WhiteElo, null as BlackElo, null as ECO";
             }
-    
+        } else if (isset($black)) {
+            if (sizeof($blacks) > 0) {
+                for ($i = 0; $i < sizeof($blacks); $i++) {
+                    if ($i > 0) {
+                        $query .= "\nUNION distinct\n";
+                    }
+                    $updateQuery = "\$query .= 'SELECT id, moves, Event,Site, Year,Month, Day,Round, White, Black, Result, WhiteElo, BlackElo, ECO  
+                        FROM $table WHERE match(black) against(\'+" .
+                        str_replace(" ", " +", $blacks[$i][0])
+                        . "\' in boolean mode)' ;";
+                    eval($updateQuery);
+                    if (isset($minYear) && isset($maxYear) && $minYear != 1475 && $maxYear != date("Y")) {
+                        $query = $query . " and Year BETWEEN ? and ? ";
+                    }
+                    if (isset($event)) {
+                        $query = $query . " and Event like ?";
+                    }
+                    if (isset($minEco) && isset($maxEco) && $minEco != "A00" && $maxEco != "E99") {
+                        $query = $query . " and CONV(eco, 16, 10) BETWEEN CONV( ?, 16, 10) AND CONV( ?, 16, 10)";
+                    }
+                }
+                if (isset($ignore) && $ignore == "true") {
+                    for ($i = 0; $i < sizeof($blacks); $i++) {
+                        $updateQuery = "\$query .= '\nUNION distinct\nSELECT id, moves, Event,Site, Year,Month, Day,Round, White, Black, Result, WhiteElo, BlackElo, ECO  
+                            FROM $table WHERE match(white) against(\'+" .
+                            str_replace(" ", " +", $blacks[$i][0])
+                            . "\' in boolean mode)' ;";
+                        eval($updateQuery);
+                        if (isset($minYear) && isset($maxYear) && $minYear != 1475 && $maxYear != date("Y")) {
+                            $query = $query . " and Year BETWEEN ? and ? ";
+                        }
+                        if (isset($event)) {
+                            $query = $query . " and Event like ?";
+                        }
+                        if (isset($minEco) && isset($maxEco) && $minEco != "A00" && $maxEco != "E99") {
+                            $query = $query . " and CONV(eco, 16, 10) BETWEEN CONV( ?, 16, 10) AND CONV( ?, 16, 10)";
+                        }
+                    }
+                }
+            } else {
+                $query = "SELECT null as id, null as moves, null as Event,null as Site, null as Year, null as Month, null as Day, null as Round,";
+                $query .= "null as White, null as Black, null as Result, null as WhiteElo, null as BlackElo, null as ECO";
+            }
         }
+        $query = $query . " order BY year DESC,month DESC,day DESC limit 10000";
+        $result = $db->query($query);
     } else if ($_POST['searching'] == 'fulltext') {
         $query = "SELECT id, moves, Event,Site, Year,Month, Day,Round, White, Black, Result, WhiteElo, BlackElo, ECO  FROM $table WHERE ";
         $toBind = array();
@@ -212,31 +293,31 @@ if (isset($_POST['searching'])) {
                 array_push($toBind, "\$minEco", "\$maxEco");
             }
         }
+        $query = $query . " order BY year DESC,month DESC,day DESC limit 10000";
+        $searching = $db->prepare($query);
+        $toEval = "\$searching -> bind_param(\"";
+        foreach ($toBind as $param) {
+            eval("\$temp = gettype($param);");
+            if ($temp == "integer") {
+                $toEval = $toEval . "i";
+            } else if ($temp == "string") {
+                $toEval = $toEval . "s";
+            } else if ($temp == "double") {
+                $toEval = $toEval . "d";
+            }
+        }
+        $toEval = $toEval . "\"";
+        foreach ($toBind as $param) {
+            if (gettype($param) == "integer" || gettype($param) == "string" || gettype($param) == "double") {
+                $toEval = $toEval . ", $param";
+            }
+        }
+        $toEval = $toEval . ");";
+        eval($toEval);
+        $searching->execute();
+        $result = $searching->get_result();
     }
 }
-$query = $query . " order BY year DESC,month DESC,day DESC limit 10000";
-$searching = $db->prepare($query);
-$toEval = "\$searching -> bind_param(\"";
-foreach ($toBind as $param) {
-    eval("\$temp = gettype($param);");
-    if ($temp == "integer") {
-        $toEval = $toEval . "i";
-    } else if ($temp == "string") {
-        $toEval = $toEval . "s";
-    } else if ($temp == "double") {
-        $toEval = $toEval . "d";
-    }
-}
-$toEval = $toEval . "\"";
-foreach ($toBind as $param) {
-    if (gettype($param) == "integer" || gettype($param) == "string" || gettype($param) == "double") {
-        $toEval = $toEval . ", $param";
-    }
-}
-$toEval = $toEval . ");";
-eval($toEval);
-$searching->execute();
-$result = $searching->get_result();
 while ($row = $result->fetch_assoc()) {
     $row['table'] = str_replace("_games", "", $table);
     array_push($data["rows"], $row);
@@ -244,7 +325,3 @@ while ($row = $result->fetch_assoc()) {
 
 print_r(json_encode($data));
 $db->close();
-
-
-
-
