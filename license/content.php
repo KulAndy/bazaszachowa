93bcea0a92fa9f87223c061434a1b4e69bae5187
Andrzej Kulesza <andykrk22@gmail.com>
2022-10-23 13:42:19 +0200

    version 2.02d

--- license/content.php
+++ license/content.php
@@ -1,18 +1,21 @@
 <div id="content">
     <article">Całość strony jest udostępniona na następujących zasadach:
-        <ul>
-            <li>użytkownik ma prawo używać strony w dowolnym celu</li>
-            <li>użytkownik ma prawo do analizowania strony*</li>
-            <li>użytkownik ma prawo do kopiowania strony</li>
-            <li>użytkownik ma prawo do udoskonalania i publicznego rozpowszechniania ulepszeń strony*</li>
-            <li>użytkownik nie może zmienić licencji</li>
-            <li>użytkownik nie może pobierać opłat za projekt, w którym został wykorzystany jakikolwiek element z tej strony</li>
-            <li>użytkownik jeśli skorzysta z wyszukiwarki, to ma obowiązek sprawdzenia swoich gier na dany moment z minimum
+        <ul>użytkownik
+            <li> ma prawo używać strony w dowolnym celu</li>
+            <li> ma prawo do analizowania strony*</li>
+            <li> ma prawo do kopiowania strony*</li>
+            <li> ma prawo do udoskonalania i publicznego rozpowszechniania ulepszeń strony*</li>
+            <li> nie może zmienić licencji</li>
+            <li> nie może pobierać opłat za projekt, w którym został wykorzystany jakikolwiek element z tej
+                strony*</li>
+            <li> jeśli skorzysta z wyszukiwarki, to ma obowiązek sprawdzenia swoich gier na dany moment z
+                minimum
                 ostatnich 3 lat przynajmniej z bazy z której korzystał i zgłoszenia ewentualnych błędów </li>
-            <li>jeśli użytkownik osiągnie jakąkolwiek korzyść z korzystania z tej strony, to zobowiązuje się postawić piwo autorowi strony</li>
+            <li> zobowiązuje się postawić piwo autorowi strony</li>
         </ul>
         <sub>* kod źródłowy dostępny na <a href="https://github.com/KulAndy/bazaszachowa">githubie</a></sub>
         <hr>
-        we wszystkich innych przypadkach obowiązuje licencja <a href="https://www.gnu.org/licenses/gpl-3.0.html">GPLv3</a>
+        we wszystkich innych przypadkach obowiązuje licencja <a
+            href="https://www.gnu.org/licenses/gpl-3.0.html">GPLv3</a>
         </article>
 </div>
