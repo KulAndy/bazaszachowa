0d52be3702ee5243c1af03c3f1ca62ae365ce65f
KulAndy <83453405+KulAndy@users.noreply.github.com>
2023-03-12 19:39:56 +0100

    Create settings.js

---
+++ app/settings.js
@@ -0,0 +1,27 @@
+const SETTINGS = {
+  admin: "",
+  admin_contact: "",
+  host: "",
+  user: "",
+  password: "",
+  base: "",
+  all_table: "",
+  poland_table: "",
+  all_players: "",
+  poland_players: "",
+  events_table: "",
+  sites_table: "",
+  rounds_table: "",
+  results_table: "",
+  eco_table: "",
+  mail_server: "",
+  mail_user: "",
+  mail_password: "",
+  mail_port: {
+    smtp: [25, 465, 587],
+    imap: [143, 993],
+    pop3: [110, 995],
+  },
+};
+
+module.exports = SETTINGS;
