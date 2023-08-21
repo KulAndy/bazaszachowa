0fdb33c17a9b989652b7d59b2a96c21f9191ae78
KulAndy <83453405+KulAndy@users.noreply.github.com>
2023-08-21 22:56:11 +0200

    version 5.0.4

--- rodo/content.php
+++ rodo/content.php
@@ -2,4 +2,11 @@
         nagrania w każdym inny sporcie,
         trafia on ( razem z nazwiskami czy wynikami ) do domeny publicznej. Co za tym idzie wszyscy mają do niego pełne
         prawa.
+        <br>
+        <br>
+        Z dedykacją dla was zamieszczam tę piosenkę.
+        <br>
+        <video src="/video/Szymon Podwin - _Całujcie mnie wszyscy w dupę_ (Julian Tuwim).mp4" autoplay loop
+            controls></video>
+        <figcaption>Szymon Podwin - "Całujcie mnie wszyscy w dupę" (Julian Tuwim) </figcaption>
     </article>
