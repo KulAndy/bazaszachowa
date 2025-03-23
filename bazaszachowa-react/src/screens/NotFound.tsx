53daa55240011d31485d0c64e50ad33d0d982413
Andrzej Kulesza <andykrk22@gmail.com>
2025-03-23 14:19:43 +0100

    fix not found on game pgn

--- bazaszachowa-react/src/screens/NotFound.tsx
+++ bazaszachowa-react/src/screens/NotFound.tsx
@@ -1,5 +1,16 @@
+import { useLocation, useParams } from "react-router-dom";
 import Content from "../components/Content";
+import { NOMENU_URLS } from "../settings";
 const NotFound = () => {
+  const location = useLocation();
+  const { base, gameid } = useParams();
+  if (
+    location.pathname.startsWith(`${NOMENU_URLS.game_raw}`) &&
+    base &&
+    gameid
+  ) {
+    window.location.reload();
+  }
   return (
     <Content>
       <h1 className="error">Nie znaleziono strony</h1>
