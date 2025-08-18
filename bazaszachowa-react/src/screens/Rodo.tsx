2beec7f1cab731d5a72cb3ff8081e3ae81239c84
Andrzej Kulesza <andykrk22@gmail.com>
2025-08-18 20:41:33 +0200

    translate rodo

--- bazaszachowa-react/src/screens/Rodo.tsx
+++ bazaszachowa-react/src/screens/Rodo.tsx
@@ -1,27 +1,23 @@
 import Content from "../components/Content";
+import { useI18n } from "../i18n/I18nContext";
 
 const Rodo = () => {
+  const { t } = useI18n();
   return (
     <Content>
       <article>
-        Drodzy fani RODO, wasze partie nie zostaną usunięte, bo w momencie
-        opublikowania zapisu partii, tak jak nagrania w każdym inny sporcie,
-        trafia on ( razem z nazwiskami czy wynikami ) do domeny publicznej. Co
-        za tym idzie wszyscy mają do niego pełne prawa.
+        {t("gdpr_info")}
         <br />
         <br />
-        Z dedykacją dla was zamieszczam tę piosenkę.
+        {t("gdpr_dedication")}
         <br />
         <br />
         <iframe
           allow="autoplay"
-          src="/Szymon-Podwin-_Całujcie-mnie-wszyscy-w-dupę_-_Julian-Tuwim_.mp3"
-          title="całujcie mnie wszyscy w dupę"
+          src={t("kiss_my_ass_link")}
+          title={t("kiss_my_ass_title")}
         ></iframe>
-        <figcaption>
-          Szymon Podwin - &quot;Całujcie mnie wszyscy w dupę&quot; (Julian
-          Tuwim)
-        </figcaption>
+        <figcaption>{t("kiss_my_ass_caption")} </figcaption>
       </article>
     </Content>
   );
