a76affab17c9e2921dce402cc39b17d19f206426
Andrzej Kulesza <andykrk22@gmail.com>
2025-08-19 17:07:02 +0200

    translate not found

--- bazaszachowa-react/src/screens/NotFound.tsx
+++ bazaszachowa-react/src/screens/NotFound.tsx
@@ -1,8 +1,10 @@
 import Content from "../components/Content";
+import { useI18n } from "../i18n/I18nContext";
 const NotFound = () => {
+  const { t } = useI18n();
   return (
     <Content>
-      <h1 className="error">Nie znaleziono strony</h1>
+      <h1 className="error">{t("not_found")}</h1>
     </Content>
   );
 };
