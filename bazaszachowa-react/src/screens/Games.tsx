b72da0d87e20a1f7c2426f2ba4b64f11e2d88f6d
Andrzej Kulesza <andykrk22@gmail.com>
2025-08-19 11:11:09 +0200

    translate games

--- bazaszachowa-react/src/screens/Games.tsx
+++ bazaszachowa-react/src/screens/Games.tsx
@@ -5,9 +5,11 @@ import { GameData } from "../ChessEditor";
 import Content from "../components/Content";
 import GamesTable from "../components/GamesTable";
 import SearchPlayersWithHints from "../components/SearchPlayersWithHint";
+import { useI18n } from "../i18n/I18nContext";
 import { API } from "../settings";
 
 const Games = () => {
+  const { t } = useI18n();
   const currentYear = new Date().getFullYear();
   const [white, setWhite] = useState("");
   const [black, setBlack] = useState("");
@@ -25,29 +27,19 @@ const Games = () => {
 
   const options1 = [];
   const options2 = [];
-  const letters = ["A", "B", "C", "D", "E"];
 
   let counter = 1;
 
-  for (const letter of letters) {
+  for (const letter of ["A", "B", "C", "D", "E"] as const) {
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
         options1.push(
-          <option value={counter++}>
+          <option value={counter}>
             {letter}
             {i}
             {j}
           </option>,
         );
-      }
-    }
-  }
-
-  counter = 1;
-
-  for (const letter of letters) {
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
         options2.push(
           <option value={counter++}>
             {letter}
@@ -101,7 +93,9 @@ const Games = () => {
           <form onSubmit={handleSubmit}>
             <table className="no_border">
               <tr>
-                <td>Białe:</td>
+                <td>
+                  <label>{t("white")}:</label>
+                </td>
                 <td colSpan={3}>
                   <SearchPlayersWithHints
                     f={setWhite}
@@ -113,7 +107,9 @@ const Games = () => {
                 </td>
               </tr>
               <tr>
-                <td>Czarne: </td>
+                <td>
+                  <label>{t("black")}:</label>
+                </td>
                 <td colSpan={3}>
                   <SearchPlayersWithHints
                     f={setBlack}
@@ -125,7 +121,9 @@ const Games = () => {
                 </td>
               </tr>
               <tr>
-                <td style={{ width: "21ch" }}>ignoruj kolory</td>
+                <td style={{ width: "21ch" }}>
+                  <label>{t("games.ignore_colors")}</label>
+                </td>
                 <td colSpan={3}>
                   <input
                     checked={ignore}
@@ -137,7 +135,9 @@ const Games = () => {
                 </td>
               </tr>
               <tr>
-                <td>lata:</td>
+                <td>
+                  <label>{t("years")}:</label>
+                </td>
                 <td style={{ display: "flex", justifyContent: "flex-end" }}>
                   <input
                     max={currentYear}
@@ -167,7 +167,9 @@ const Games = () => {
                 </td>
               </tr>
               <tr>
-                <td>turniej:</td>
+                <td>
+                  <label>{t("tournament")}:</label>
+                </td>
                 <td colSpan={3}>
                   <input
                     onChange={(e) => {
@@ -205,9 +207,11 @@ const Games = () => {
                 </td>
               </tr>
               <tr>
-                <td>baza:</td>
                 <td>
-                  Polska{" "}
+                  <label>{t("base")}:</label>
+                </td>
+                <td>
+                  <label>{t("games.poland")} </label>
                   <input
                     checked={base === "poland"}
                     name="base"
@@ -219,8 +223,7 @@ const Games = () => {
                   />
                 </td>
                 <td colSpan={2}>
-                  {" "}
-                  całość{" "}
+                  <label>{t("games.all")} </label>
                   <input
                     checked={base === "all"}
                     name="base"
@@ -233,9 +236,11 @@ const Games = () => {
                 </td>
               </tr>
               <tr>
-                <td style={{ width: "18ch" }}>wyszukiwanie</td>
+                <td style={{ width: "18ch" }}>
+                  <label>{t("games.searching")}</label>
+                </td>
                 <td>
-                  zwykłe
+                  <label>{t("games.searching_classic")}</label>
                   <input
                     checked={searching === "classic"}
                     name="searching"
@@ -246,7 +251,7 @@ const Games = () => {
                   />
                 </td>
                 <td colSpan={2}>
-                  dokładne
+                  <label>{t("games.searching_exact")}</label>
                   <input
                     checked={searching === "fulltext"}
                     name="searching"
@@ -259,50 +264,33 @@ const Games = () => {
               </tr>
               <tr style={{ height: "4em" }}>
                 <th colSpan={4}>
-                  <button>szukaj</button>
+                  <button>{t("games.search")}</button>
                 </th>
               </tr>
             </table>
           </form>
           <div id="right_content">
             <details id="help">
-              <summary>Pomoc</summary>
+              <summary>{t("games.help")}</summary>
               <ul>
-                Możliwe parametry wyszukiwania:
-                <li>Trzeba podać nazwisko przynajmniej jednego gracza</li>
+                {t("games.params")}
+                <li>{t("games.param.player")}</li>
                 <li>
-                  kody
-                  <a href="https://pl.wikipedia.org/wiki/Encyklopedia_otwar%C4%87_szachowych">
-                    ECO
-                  </a>
-                  na wikipedii
+                  {t("games.param.eco")}{" "}
+                  <a href={t("eco_href")}>{t("games.param.ecoLink")}</a>
                 </li>
                 <li>
+                  {t("games.param.database")}
                   <ul>
-                    baza
-                    <li>
-                      Polska - turnieje głównie z Polski, ok 700 tys. , szybsza
-                      baza
-                    </li>
-                    <li>
-                      Całość - wszystkie partie, ok 15 mln, wolniejsza baza
-                    </li>
+                    <li>{t("games.param.database.pl")}</li>
+                    <li>{t("games.param.database.all")}</li>
                   </ul>
                 </li>
                 <li>
+                  {t("games.param.search")}
                   <ul>
-                    Wyszukiwanie (wielkość liter nie ma znaczenia)
-                    <li>
-                      Zwykłe - zadziała zarówno wpisanie &quot;Nowak, Jan&quot;
-                      jak i &quot;Nowak, J&quot; , można stować jokery (
-                      &quot;_&quot; - jeden dowolny znak, &quot;%&quot; -
-                      dowolny ciąg znaków )
-                    </li>
-                    <li>
-                      Dokładne - wyszuka tylko po wpisaniu &quot;Nowak,
-                      Jan&quot;, szybsze, zalecane jeśli zna się pełne imię i
-                      nazwisko gracza/y
-                    </li>
+                    <li>{t("games.param.search.normal")}</li>
+                    <li>{t("games.param.search.exact")}</li>
                   </ul>
                 </li>
               </ul>
@@ -313,7 +301,7 @@ const Games = () => {
           <div>
             <div className="loading">
               <div className="spin"></div>
-              <p>Ładowanie gier ... </p>
+              <p>{t("games.loading_games")} </p>
             </div>
           </div>
         ) : (
