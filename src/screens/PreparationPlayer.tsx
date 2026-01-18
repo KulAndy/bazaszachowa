698fda2a86091fd298397922131135fa433232d2
Andrzej Kulesza <andykrk22@gmail.com>
2026-01-18 12:38:23 +0100

    node independent setTimeout type

--- src/screens/PreparationPlayer.tsx
+++ src/screens/PreparationPlayer.tsx
@@ -17,7 +17,7 @@ const debounce = <T extends unknown[]>(
   callback: (...arguments_: T) => void,
   delay: number,
 ): ((...arguments_: T) => void) => {
-  let timeoutId: number | undefined;
+  let timeoutId: ReturnType<typeof setTimeout> | undefined;
 
   return (...arguments_: T): void => {
     clearTimeout(timeoutId);
