97d9daa47f6e9b0170a985cd76eb717bc392d3b6
Andrzej Kulesza <andykrk22@gmail.com>
2026-05-10 10:15:54 +0200

    correct elo converting

--- src/screens/PZSzachCalculator/index.tsx
+++ src/screens/PZSzachCalculator/index.tsx
@@ -52,11 +52,13 @@ const getRatingRanges = (sex: "F" | "M") => {
     ),
   ).toSorted((a, b) => b - a);
 
-  return ratings.map((rating, index) => ({
-    max: index === 0 ? null : ratings[index - 1] - 1,
-    min: index === ratings.length - 1 ? null : rating,
-    rating,
-  }));
+  return ratings
+    .map((rating, index) => ({
+      max: index === 0 ? null : rating,
+      min: index >= ratings.length - 2 ? null : ratings[index + 1] + 1,
+      rating,
+    }))
+    .slice(0, -1);
 };
 
 const femaleRatingRanges = getRatingRanges("F");
