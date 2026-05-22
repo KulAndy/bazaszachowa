c55739569be4c3c1515b33819eacdf8ef94bbe9b
Andrzej Kulesza <andykrk22@gmail.com>
2026-05-22 08:28:37 +0200

    major update dpes

--- vite.config.ts
+++ vite.config.ts
@@ -9,19 +9,7 @@ export default defineConfig({
     sourcemap: false,
   },
   plugins: [
-    react({
-      babel: {
-        presets: ["@babel/preset-typescript"],
-        plugins: [
-          "babel-plugin-react-compiler",
-          "babel-plugin-console-source",
-          "@babel/plugin-transform-strict-mode",
-          "@babel/plugin-transform-json-strings",
-          "@babel/plugin-transform-literals",
-          "@babel/plugin-transform-unicode-regex",
-        ],
-      },
-    }),
+    react(),
     checker({
       typescript: true,
       eslint: {
